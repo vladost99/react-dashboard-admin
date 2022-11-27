c024efe55d5617a6203025ef2746dcdf1b67cf27
Vlad <vladsa000@gmail.com>
2022-11-27 17:38:25 +0200

    fix styles topbar and added contacts page

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -16,7 +16,7 @@ const Topbar = () => {
   return (
     <Box display='flex' justifyContent="space-between" p={2}>
 
-        <Box display='flex' backgroundColor={colors.primary[400]} borderRadious="3px">
+        <Box display='flex' backgroundColor={colors.primary[400]} borderRadius="3px">
           <InputBase sx={{ml: 2, flex: 1}} placeholder='Search' />
           <IconButton type='button' sx={{p: 1}}>
               <SearchIcon/>
