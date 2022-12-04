baa5621848ff2c0a5c6bedcfcc7eb90a76a20ed6
Vlad <vladsa000@gmail.com>
2022-12-04 11:58:02 +0200

    added page FAQ

---
+++ src/scenes/faq/index.js
@@ -0,0 +1,97 @@
+import Header from "components/Header";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import useColorTheme from "hooks/useColorTheme";
+
+const FAQ = () => {
+  const { colors } = useColorTheme();
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+            minima molestiae error, suscipit minus facilis maxime sit pariatur.
+            Recusandae, asperiores!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+            minima molestiae error, suscipit minus facilis maxime sit pariatur.
+            Recusandae, asperiores!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+          Your favorite Qeustion
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+            minima molestiae error, suscipit minus facilis maxime sit pariatur.
+            Recusandae, asperiores!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Some Random Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+            minima molestiae error, suscipit minus facilis maxime sit pariatur.
+            Recusandae, asperiores!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+           The Final Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+            minima molestiae error, suscipit minus facilis maxime sit pariatur.
+            Recusandae, asperiores!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+};
+
+export default FAQ;
